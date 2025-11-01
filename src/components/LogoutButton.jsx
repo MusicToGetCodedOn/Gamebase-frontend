95defa039a905005b700fa5a7a99f245ae46b40a
Loris Perez <lpe149399@stud.gibb.ch>
2025-11-01 13:55:51 +0100

    again and again and again i am so desperate right now

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import logout from "../assets/icons/logout.png";
-import "./Header.css";
+import "./Logoutbtn.css";
 
 export default function LogoutButton() {
   const navigate = useNavigate();
